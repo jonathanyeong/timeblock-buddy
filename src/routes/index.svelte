7821459f3b540c0c957f6b61a1e0ce8a8c111aa2
Jonathan Yeong <jonathanyeong@gmail.com>
2021-06-29 16:59:43 -0700

    Add some (terrible) styling
    Trying to convert the checkboxes into a clickable box.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,39 @@
 <script context="module">
   import { createClient } from '@supabase/supabase-js'
+  import { formatISO, set, format } from 'date-fns';
 
   const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
   const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
-  // Delete the git history.
 
   const supabase = createClient(supabaseUrl, supabaseKey)
 
 	export function load() {
 		return {
 			props: {
-				supabase: supabase
+				supabase: supabase,
+        timeblocks: setTimeblocks()
 			}
 		};
 	}
+
+  const setTimeblocks = () => {
+    const numTimeblocks = 10; // 10 hours in the day
+
+    let timeblocks = []
+
+    const startTime = 8 // 0800 hours
+    for (let i = 0; i < numTimeblocks; i++) {
+      const date = set(new Date(), {hours: startTime+i, minutes: 0, seconds: 0 })
+      let newBlock = { id: format(date, 'h:ms aaa'), timestamp: formatISO(date), text: format(date, 'h:ms aaa'), checked: false };
+      timeblocks.push(newBlock);
+    }
+    return timeblocks;
+  }
 </script>
 
 <script>
-  import { formatISO } from 'date-fns';
   export let supabase;
+  export let timeblocks = []
   let projectName;
 
   const saveBlock = async function(projectName, timeblocks) {
@@ -45,27 +60,51 @@
     saveBlock(projectName, timeblocks)
   }
 
-  const timeblocks = [
-    { id: "0900", timestamp: formatISO(new Date(2021, 5, 24, 9, 0, 0)), text: "9:00am", checked: false },
-    { id: "1000", timestamp: formatISO(new Date(2021, 5, 24, 10, 0, 0)), text: "10:00am", checked: false },
-    { id: "1100", timestamp: formatISO(new Date(2021, 5, 24, 11, 0, 0)), text: "11:00am", checked: false },
-    { id: "1200", timestamp: formatISO(new Date(2021, 5, 24, 12, 0, 0)), text: "12:00am", checked: false },
-    { id: "1300", timestamp: formatISO(new Date(2021, 5, 24, 13, 0, 0)), text: "1:00pm", checked: false }
-  ]
-
 </script>
 
 <h1>Timeblock buddy</h1>
 
 <form>
+  <div class="dayView">
+    {#each timeblocks as timeblock}
+      <div class="timeblock">
+        <label for="{timeblock.id}">{timeblock.text}</label>
+        <input id="{timeblock.id}" type="checkbox" bind:checked={timeblock.checked}>
+      </div>
+    {/each}
+  </div>
 
-
-  {#each timeblocks as timeblock}
-    <label for="{timeblock.id}">{timeblock.text}</label>
-    <input id="{timeblock.id}" type="checkbox" bind:checked={timeblock.checked}>
-  {/each}
 
   <label for="projectName">Project Name</label>
   <input id="projectName" type="text" bind:value={projectName}>
   <input on:click|preventDefault={handleSubmit} type="submit" value="Submit">
 </form>
+
+<style>
+  .dayView {
+    width: 150px;
+    display: flex;
+    flex-direction: column;
+    background-color: #9CA3AF;
+  }
+  .timeblock {
+    height: 100px;
+    position: relative;
+  }
+
+  .timeblock label {
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+    height: 1px;
+    width: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+  }
+
+  .timeblock input {
+    width: 100%;
+    height: 100px;
+  }
+</style>
