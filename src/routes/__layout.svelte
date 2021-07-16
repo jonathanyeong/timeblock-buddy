3866bfd2773134fc7f27ce896539e517c24cb97f
Jonathan Yeong <jonathanyeong@gmail.com>
2021-07-16 09:21:11 -0700

    Add tailwind and import the styles in layout
    This will enable tailwind css classes for any components/files I reference in
    routes.

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import '../tailwind.css'
+</script>
+
+<h1 class="text-5xl mb-8">Timeblock buddy</h1>
+
+<slot></slot>
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
